81b64affc4a581db676577b4703ac7814ade533e
federicorotolo (Mr ) <federico_rotolo@libero.it>
2011-11-17 09:28:19 +0000

    improved html page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/parfm@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,7 +1,3 @@
-
-<!-- This is the project specific website template -->
-<!-- It can be changed as liked or replaced by other content -->
-
 <?php
 
 $domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
@@ -19,6 +15,8 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?php echo $group_name; ?></title>
 	<link href="http://<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+  <meta name="description" content="Parametric frailty models in R" />
+  <meta name="keywords" content="parametric, frailty, frailty models, survival, R, package" /> 
   </head>
 
 <body>
@@ -30,26 +28,42 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 </table>
 
 
-<!-- get project title  -->
-<!-- own website starts here, the following may be changed as you like -->
+<!-- project title  -->
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
+<h2>Parametric Frailty Models in R</h2>
 
-<!-- end of project description -->
+<p>Frailty Models [1,2] are survival models for clustered or overdispersed duration data.
+  They consist in proportional hazards Cox models [3] with the addition of a random effect, accounting for different levels of risk
+    due to unobserved covariates.
+<p>Accoridng to the focus of the interest, estimation of parameters can be done by means of either a parametric or a semiparametric model.
+  In the latter case, the baseline hazard is left unspecified and the penalized partial likelihood is considered [4].
+  Estimation can be done in R by means of the
+    <a href="http://stat.ethz.ch/R-manual/R-patched/library/survival/html/coxph.html"><tt>coxph</tt></a> function 
+    in the <a href="http://cran.r-project.org/web/packages/survival/index.html"><tt>survival</tt></a> package.
+
+<p>Here we provide code for estimating frailty models, based on the marginal likelihood, for many of the most common models.
+  Possible basline hazards are
+  <ul> <li>Weibull,</li> <li>Exponential,</li> <li>Gompertz,</li> <li>logNormal,</li> <li>loglogistic.</li> </ul>
+ Possible Frailty distributions are
+  <ul> <li>Gamma,</li> <li>Inverse Gaussian,</li> <li>Positive Stable.</li> </ul>
+The method is analogous to that of the Stata
+  <tt><a href = "http://www.stata-journal.com/article.html?article=st0006">streg</a></tt> command [5],
+  the difference being that <tt>streg</tt> offers one more baseline (the generalized gamma survival distribution)
+  and one less frailty distribution (the Positive Stable).
+
 
 <h3>References</h3>
-<ul>
- <li> Duchateau, L. & Janssen, P. (2008) <em>The frailty model</em>. Springer.</li>
- <li> Wienke, A. (2010) <em>Frailty Models in Survival Analysis</em>. Chapman & Hall/CRC biostatistics series. Taylor and Francis.</li>
-</ul>
+<p> [1] Duchateau, L. & Janssen, P. (2008) <em>The frailty model</em>. Springer.</p>
+<p> [2] Wienke, A. (2010) <em>Frailty Models in Survival Analysis</em>. Chapman & Hall/CRC biostatistics series. Taylor and Francis.</p>
+<p> [3] Cox, D. R. (1972) Regression models and life-tables. 
+  <em>Journal of the Royal Statistical Society. Series B (Methodological)</em> 34, 187–220.</p>
+<p> [4] Therneau, T. M., Grambsch, P. M. & Pankratz, V. S. (2003) Penalized survival models and frailty.
+  <em>Journal of Computational and Graphical Statistics</em> 12, 156–175.</p>
+<p> [5] Gutierrez, R. G.  (2002) Parametric frailty and shared frailty survival models. 
+  <em>Stata Journal</em> 2, 22-44.</p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p>
+<p> The <tt>project summary page</tt> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><tt>here</tt></a>. </p>
 
 </body>
 </html>
