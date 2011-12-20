90778738643f137aafae2f16935ac4de71eb3e6e
federicorotolo (Mr ) <federico_rotolo@libero.it>
2011-12-20 17:08:44 +0000

    added Mloglikelihood
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/parfm@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/Mloglikelihood.R
@@ -0,0 +1,150 @@
+################################################################################
+#  Minus the log-likelihood                                                    #
+################################################################################
+#                                                                              #
+#  This function computes minus the logarithm of the likelihood function       #
+#                                                                              #
+#  Its parameters are                                                          #
+#   - p      : the parameters vector, in the form                              #
+#              c( frailty distribution parameter(s),                           #
+#                 baseline hazard parameter(s),                                #
+#                 regression parameter(s) )                                    #
+#   - obs    : the observed data, in the form                                  #
+#              list( time   = event/censoring times,                           #
+#                   [trunc  = left truncation times, ]                         #
+#                    event   = event idnicators,                               #
+#                    x       = covariate data.frame,                           #
+#                    cluster = cluster ID vector,                              #
+#                    ncl     = number of clusters,                             #
+#                    di      = number of events per cluster                    #
+#   - dist   : the baseline hazard distribution name                           #
+#   - frailty: the frailty distribution name                                   #
+#                                                                              #
+#                                                                              #
+#   Date: December, 19, 2012                                                   #
+#                                                                              #
+################################################################################
+#   Check status: still to check                                               #
+#   Comments:                                                                  #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#   On date:                                                                   #
+################################################################################
+
+Mloglikelihood <- function(p,
+                           obs,
+                           dist,
+                           frailty) { 
+  # ---- Assign the number of frailty parameters 'nFpar' ---------------------#
+  # ---- and compute the polynomials Omega for the Positive Stable frailty ---#
+  
+  if (frailty == "none") 
+    nFpar <- 0
+  else if (frailty %in% c("gamma","ingau")) {
+    theta <- exp(p[1])
+    nFpar <- 1 
+  }
+  else if (frailty == "possta") {
+    theta <- exp(-exp(p[1]))
+    nFpar <- 1
+    D <- max(obs$di)
+    Omega <- matrix(NA, nrow=D+1, ncol=D)
+    Omega[, 1] <- rep(1, D+1)
+    
+    if (D==2) { Omega[3,2] <- 1/theta - 1 } else if (D>2) {
+      for(q in 3:(D+1))
+        Omega[q, q-1] <- theta^(2 - q) * prod(q - theta - seq(2, q-1, 1))
+      for(m in 2:(D-1))
+        for(q in (m+2):(D+1))
+          Omega[q, m] <- Omega[q-1, m] + 
+            (Omega[q-1, m-1] * ((q - 2) / theta - (q - m)))
+    }
+  }
+  
+    
+  # ---- Baseline hazard -----------------------------------------------------#
+  
+  # Reparametrisation and naming of baseline parameters
+  if (dist == "weibull") { 
+    pars <- c(rho    = exp(p[nFpar+1]),
+              lambda = exp(p[nFpar+2]))
+    beta <- p[-(1:(nFpar+2))]
+  } else
+  if (dist == "exponential") {
+    pars <- c(lambda = exp(p[nFpar+1]))
+    beta <- p[-(1:(nFpar+1))]
+  } else
+  if (dist == "gompertz") {
+    pars <- c(gamma  = exp(p[nFpar+1]),
+              lambda = exp(p[nFpar+2]))    
+    beta <- p[-(1:(nFpar+2))]
+  } else
+  if (dist == "lognormal") {
+    pars <- c(mu    = p[nFpar+1],
+              sigma = exp(p[nFpar+2]))    
+    beta <- p[-(1:(nFpar+2))]
+  } else
+  if (dist == "loglogistic") {
+    pars <- c(alpha = p[nFpar+1],
+              kappa = exp(p[nFpar+2]))    
+    beta <- p[-(1:(nFpar+2))]
+  }
+  
+  # baseline: from string to the associated function
+  dist <- eval(parse(text=dist))
+
+    
+  # ---- Cumulative Hazard by cluster ----------------------------------------#
+  
+  cumhaz <- NULL
+  cumhaz <- aggregate(
+      dist(pars, obs$time, what="H") *
+      if (is.null(obs$x)) 1 else exp(as.matrix(obs$x) %*% beta), 
+    by=list(obs$cluster), FUN=sum)[, 2]
+  
+  # Possible truncation
+  if (!is.null(obs$trunc)) cumhaz <- cumhaz - aggregate(
+      dist(pars, obs$trunc, what="H") *
+      if (is.null(obs$x)) 1 else exp(as.matrix(obs$x) %*% beta), 
+    by=list(obs$cluster), FUN=sum)[, 2]
+
+    
+  # ---- 1: log-hazard -------------------------------------------------------#
+  
+  loghaz <- NULL
+  loghaz <- aggregate(obs$event * (dist(pars, obs$time, what="lh") + 
+    if (is.null(obs$x)) 0 else (as.matrix(obs$x) %*% beta)),
+    by=list(obs$cluster), FUN=sum)[, 2]
+
+    
+  # ---- 2: log-Survival -----------------------------------------------------#
+    
+  logSurv <- NULL
+  if (frailty=="none")
+    logSurv <-mapply(fr.none, cumhaz, what="logLT") else
+  if (frailty=="gamma")
+    logSurv <- mapply(fr.gamma, 
+                      k=obs$di, s=cumhaz, theta=rep(theta, obs$ncl), 
+                      what="logLT") 
+  else if (frailty=="ingau")
+    logSurv <- mapply(fr.ingau, 
+                      k=obs$di, s=cumhaz, theta=rep(theta, obs$ncl), 
+                      what="logLT") 
+  else if (frailty=="possta"){
+    logSurv <- rep(NA, obs$ncl)
+    logSurv <- sapply(1:obs$ncl, 
+                      function(x) fr.possta(k=obs$di[x], s=cumhaz[x], 
+                                            theta=theta, Omega=Omega, 
+                                            what="logLT") )
+  }
+
+    
+  # ---- Minus the log likelihood --------------------------------------------#
+  
+  Mloglik <- -sum(loghaz + logSurv)
+  return(Mloglik)
+}
+
