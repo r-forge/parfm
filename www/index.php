8866942b4e5232d13133ca2227514b2c9492c79a
federicorotolo (Mr ) <federico_rotolo@libero.it>
2012-10-16 14:45:19 +0000

    new webpage with lognormal frailties
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/parfm@99 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -47,10 +47,20 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <p>When the form of the baseline hazard is somehow known in advance, the parametric estimation approach can be used advantageously.
 The <b>parfm</b> package provides a wide range of parametric frailty models in <b>R</b>.
 The
-  <ul> <li>Gamma,</li> <li>Inverse Gaussian and</li> <li>Positive Stable</li> </ul>
+  <ul>
+    <li>gamma,</li> 
+    <li>inverse Gaussian,</li> 
+    <li>positive stable and</li> 
+    <li>lognormal <img src="./new.png"></li>
+  </ul>
   frailty distributions can be specified,
   together with five different baseline hazard families:
-  <ul> <li>Weibull,</li> <li>Exponential,</li> <li>Gompertz,</li> <li>Lognormal,</li> <li>Loglogistic.</li> </ul>
+  <ul>
+    <li>Weibull,</li>
+    <li>exponential,</li>
+    <li>Gompertz,</li>
+    <li>lognormal,</li>
+    <li>loglogistic.</li> </ul>
   
 <p>Parameter estimation is done by maximising the log-likelihood,
   with right-censored and possibly left-truncated data.
@@ -58,7 +68,7 @@ The
 
 <h3>Parametrisations</h3>
 <h4>Baseline hazards</h4>
-<p>The <B>Exponential</B> model is</p>
+<p>The <B>exponential</B> model is</p>
   <p align="center"><i>h</i>(<i>t; &lambda;</i>)<i> = &lambda;</i>,</p>
   <p>with <i>&lambda;&gt;0</i>.</p>
 <p>The <B>Weibull</B> model is</p>
@@ -67,30 +77,33 @@ The
 <p>The <B>Gompertz</B> model is</p>
   <p align="center"><i>h</i>(<i>t; &gamma;, &lambda;</i>)<i> = &lambda; </i>e<i><sup>&gamma;t</sup></i>,</p>
   <p>with <i>&gamma;,&lambda;&gt;0</i>.</p>
-<p>The <B>Lognormal</B> model is</p>
+<p>The <B>lognormal</B> model is</p>
   <p align="center"><i>h</i>(<i>t; &mu;, &sigma;</i>) = 
     {<i> &phi;</i>([log<i> t -&mu;</i>]<i>/&sigma;</i>)}<i> / </i>{<i> &sigma; t </i>[<i>1-&Phi;</i>([log<i> t -&mu;</i>]<i>/&sigma;</i>)]},</p>
   <p>with <i>&mu;&in;<strong>R</strong></i>, <i>&sigma;&gt;0</i> and <i>&phi;</i>(<i>.</i>)<i></i> and <i>&Phi;</i>(<i>.</i>)<i></i> the density and distribution functions of a standard Normal.</p>
-<p>The <B>Loglogistic</B> model is</p>
+<p>The <B>loglogistic</B> model is</p>
   <p align="center"><i>h</i>(<i>t; &alpha;, &kappa;</i>) = 
     {exp(<i>&alpha;</i>) <i>&kappa; t<sup>&kappa;-1</sup> } <i>/</i> {
       <i>1 +</i> exp(<i>&alpha;</i>) <i>t<sup>&kappa;</sup></i>},</p>
   <p>with <i>&alpha;&in;<strong>R</strong></i> and <i>&kappa;&gt;0</i>.</p>
 
 <h4>Frailty distributions</h4>
-<p>The <B>Gamma</B> distribution is</p>
+<p>The <B>gamma</B> distribution is</p>
   <p align="center"><i>f</i>(<i>u;, &theta;</i>) = 
     {<i> u<sup>1/&theta;-1</sup> </i>e<i><sup>-u/&theta;</sup> </i>}<i>/</i>{<i> &Gamma;</i>(<i>1/&theta;</i>)<i> &theta;<sup>1/&theta;</sup> </i>}<i></i>,</p>
   <p>with <i>&theta;&gt;0</i> and <i>&Gamma;</i>(<i>.</i>)<i></i> the Gamma function.</p>
-<p>The <B>Inverse Gaussian</B> distribution is</p>
+<p>The <B>inverse Gaussian</B> distribution is</p>
   <p align="center"><i>f</i>(<i>u; &theta;</i>) = 
     (<i>2&theta;&pi;</i>)<i><sup>-1/2</sup> u<sup>-3/2</sup> </i>exp<i></i>{<i> </i>(<i>u-1</i>)<i><sup>2</sup> / 2u&theta; </i>}<i></i>,</p>
   <p>with <i>&theta;&gt;0</i>.</p>
-<p>The <B>Positive Stable</B> distribution is</p>
+<p>The <B>positive stable</B> distribution is</p>
   <p align="center"><i>f</i>(<i>u; &nu;</i>)<i> = 
     -</i>&Sigma;<i><sub>k=1...&#8734;</sub></i>{<i>-u<sup>-(1-&nu;)k</sup> </i>sin<i></i>(<i>(1-&nu;)k&pi;</i>)<i> &Gamma;</i>(<i>(1-&nu;)k+1</i>)<i>/k!</i>}<i> / &pi;u</i>,</p>
   <p>with <i>&nu;&in;(0, 1)</i> and <i>&Gamma;</i>(<i>.</i>)<i></i> the Gamma function.</p>
-
+<p>The <B>lognormal</B> distribution is</p>
+  <p align="center"><i>f</i>(<i>u; &nu;</i>)<i> = 
+    (<i>2&theta;&pi;</i>)<i><sup>-1/2</sup> u<sup>-1</sup> </i>exp<i></i>{<i> </i>(log<i>u</i>)<i><sup>2</sup> / 2&theta; </i>}<i></i>,</p>
+  <p>with <i>&theta;&gt;0</i>.</p>
 
 
 <!--References--><hr>
